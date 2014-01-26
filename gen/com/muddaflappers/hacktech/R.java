6ea08d56f3c5aa3140977ee1f91c1b6823e1335e
Dylan Nguyen <dh.dvnguyen@gmail.com>
2014-01-26 01:56:45 -0800

    gui finished?

--- gen/com/muddaflappers/hacktech/R.java
+++ gen/com/muddaflappers/hacktech/R.java
@@ -24,23 +24,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int btnGetCalendar=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int bluetooth=0x7f080007;
         public static final int dateBox=0x7f080001;
+        public static final int dateInput=0x7f080005;
+        public static final int find=0x7f080008;
+        public static final int layout_root=0x7f080002;
+        public static final int refresh=0x7f080009;
         public static final int textEnter=0x7f080000;
+        public static final int textView=0x7f080003;
+        public static final int userInput=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int prompt=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int more_menu=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int btn=0x7f050003;
-        public static final int enter=0x7f050004;
+        public static final int btn_calendar=0x7f050003;
+        public static final int btn_check_schedules=0x7f050004;
+        public static final int enter=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int refresh=0x7f050008;
+        public static final int start=0x7f050006;
+        public static final int time=0x7f050007;
+        public static final int tv_label=0x7f050009;
     }
     public static final class style {
         /** 
--- src/com/muddaflappers/hacktech/MainActivity.java
+++ src/com/muddaflappers/hacktech/MainActivity.java
@@ -9,15 +9,19 @@ import android.widget.*;
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
+import android.app.AlertDialog;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
 import android.app.Activity;
 import android.bluetooth.*;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.provider.CalendarContract;
+import android.provider.CalendarContract.Events;
 
 import java.io.File;
 import java.util.List;
@@ -28,7 +32,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity{
    
    //extra variables
    private EditText dateText;
@@ -54,156 +58,9 @@ public class MainActivity extends Activity implements OnClickListener{
      
       //set up calendar cursor
       mCursor = getContentResolver().query(CalendarContract.Events.CONTENT_URI, COLS, null, null, null);
-               
-      //set up button event
       dateText = (EditText) findViewById(R.id.dateBox);
-      Button btnGetCalendar = (Button) findViewById(R.id.btnGetCalendar);
-      btnGetCalendar.setOnClickListener(this);
-      Button btnCheckSchedules = (Button) findViewById(R.id.btnCheckSchedules);
-      btnCheckSchedules.setOnClickListener(this);
       eventListCreated = false;
    }
-   
-   public void onClick(View v){
-                
-               
-                //loop through all events and add to array list
-                switch(v.getId()){
-                case R.id.btnGetCalendar:
-                	eventList = new ArrayList<Event>();
-                    GregorianCalendar dayOf = Functions.toDate(dateText); //day user wants to find
-                	mCursor.moveToFirst();
-					while(!mCursor.isAfterLast()){
-						if(!mCursor.getString(4).equals("1")){
-							GregorianCalendar start = new GregorianCalendar();
-			                GregorianCalendar end = new GregorianCalendar();
-							start.setTimeInMillis(mCursor.getLong(1));
-							end.setTimeInMillis(mCursor.getLong(2)); // end date obj
-							boolean allDay = !mCursor.getString(3).equals("0"); //gets boolean if all day
-							Event event = new Event(mCursor.getString(0), start, end, allDay);
-							Log.d("MainActivity", "Event = " + (start.get(Calendar.MONTH)+1) + "/" + start.get(Calendar.DATE) + "/" + start.get(Calendar.YEAR));
-							if(Functions.checkIfDateMatch(event, dayOf))
-							{
-								Log.d("MainActivity", "Confirmed date");
-								eventList.add(event);
-							}
-						}
-						mCursor.moveToNext(); //moves to next event
-                    }
-					Collections.sort(eventList);
-					eventListCreated = true;
-					
-					try {
-						Functions.writeToText(eventList);
-					} catch (IOException e) {
-						// Auto-generated catch block
-						e.printStackTrace();
-					}
-					Toast t = Toast.makeText(getApplicationContext(), "All Events Added", Toast.LENGTH_LONG);
-	                 t.show();
-	                 
-	                 BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-	                 
-	                 if (btAdapter == null) {
-	                    Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_LONG).show();
-	                  }
-	                 else{
-	                     enableBlu();
-	                  }
-					break;
-					
-                case R.id.btnCheckSchedules:
-                	if(eventListCreated)
-                	{
-	                	ArrayList<Event> otherList = Functions.readFile();
-	                	boolean[] freeTime = Functions.freeTimeCalc(eventList, otherList);
-	                	
-	                	/*
-	                	 * DEBUGGER DATA BELOW
-	                	 */
-	                	String otherData = "";
-	                	Log.d("Other List Size", Integer.toString(otherList.size()));
-	                	for(int i = 0; i < otherList.size(); i++)
-	                	{
-	                		otherData += "Start: " + otherList.get(i).startDate.get(Calendar.HOUR_OF_DAY) + ":" + otherList.get(i).startDate.get(Calendar.MINUTE);
-	                		otherData += "  End:" + otherList.get(i).endDate.get(Calendar.HOUR_OF_DAY) + ":" + otherList.get(i).endDate.get(Calendar.MINUTE);
-	                		otherData += "\n";
-	                	}
-	                	Log.d("Other List", otherData);
-	                	
-	                	String eventData = "";
-	                	Log.d("Event List Size", Integer.toString(eventList.size()));
-	                	for(int i = 0; i < eventList.size(); i++)
-	                	{
-	                		eventData += "Start: " + eventList.get(i).startDate.get(Calendar.HOUR_OF_DAY) + ":" + eventList.get(i).startDate.get(Calendar.MINUTE);
-	                		eventData += "  End:" + eventList.get(i).endDate.get(Calendar.HOUR_OF_DAY) + ":" + eventList.get(i).endDate.get(Calendar.MINUTE);
-	                		eventData += "\n";
-	                	}
-	                	Log.d("Event List", eventData);
-	                	
-	                	int counter;
-	                	int startHours = 0;
-	                	int endHours = 0;
-	                	int startMinutes = 0;
-	                	int endMinutes = 0;
-	                	int streak = 0;
-	                	String freeTimeString = "";
-	                	for(counter = 0; counter < freeTime.length; counter++)
-	                	{
-	                		
-	                		if(freeTime[counter])
-	                			streak++;
-                			else
-                			{
-                				if(streak == 0)
-                					continue;
-	                			startHours = (counter-streak) / 60;
-	                			startMinutes = (counter-streak) % 60;
-	                			endHours = (counter) / 60;
-	                			endMinutes = (counter) % 60;
-	                			freeTimeString += "Start: " + startHours + ":" + startMinutes + "  End: " + endHours + ":" + endMinutes + "\n";
-	                			streak = 0;
-                			}
-	                	}
-	                	if(streak != 0)
-	                	{
-	                		startHours = (counter-streak) / 60;
-                			startMinutes = (counter-streak) % 60;
-                			endHours = (counter) / 60;
-                			endMinutes = (counter) % 60;
-                			freeTimeString += "Start: " + startHours + ":" + startMinutes + "  End: " + endHours + ":" + endMinutes + "\n";
-	                	}
-	                	Log.d("WRITETEST", freeTimeString);
-	            		FileWriter fWriter;
-	            		File sdCardFile = new File(Environment.getExternalStorageDirectory() + "/freeTime.txt");
-	            		Log.d("TAG", sdCardFile.getPath()); //<-- check the log to make sure the path is correct.
-	            		try{
-	            		     fWriter = new FileWriter(sdCardFile, false);
-	            		     fWriter.write(freeTimeString);
-	            		     fWriter.flush();
-	            		     fWriter.close();
-	            		 }catch(Exception e){
-	            		          e.printStackTrace();
-	            		 }
-	            		
-	            		intent = new Intent();
-	            		intent.setAction(Intent.ACTION_SEND);
-	            		intent.setType("text/plain");
-	            	    intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/freeTime.txt")));
-		                BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
-		                 
-		                 if (blueAdapter == null) {
-		                    Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_LONG).show();
-		                  }
-		                 else{
-		                     enableBlu();
-		                  }
-
-                	}
-                	else
-                		Toast.makeText(this, "Generate Calendar Events First!", Toast.LENGTH_LONG).show();
-                }
-        }
    
    // duration that the device is discoverable
    private static final int DISCOVER_DURATION = 300;
@@ -244,15 +101,176 @@ public class MainActivity extends Activity implements OnClickListener{
 	      }else{ // cancelled or error
 	         Toast.makeText(this,"Bluetooth Canceled", Toast.LENGTH_SHORT).show();
 	      }
-	 
 	}
    
    @Override
    public boolean onCreateOptionsMenu(Menu menu)
    {
       // Inflate the menu; this adds items to the action bar if it is present.
-      getMenuInflater().inflate(R.menu.main, menu);
+      getMenuInflater().inflate(R.menu.more_menu, menu);
       return true;
    }
- 
+   
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      final Context context = this;
+       switch(item.getItemId())
+       {
+       case R.id.bluetooth:
+          BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+          //if bluetooth not supported
+          if (btAdapter == null) {
+             Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_LONG).show();
+          }else{
+          eventList = new ArrayList<Event>();
+          
+          LayoutInflater li = LayoutInflater.from(context);
+          View promptView = li.inflate(R.layout.prompt, null);
+          AlertDialog.Builder adb = new AlertDialog.Builder(context);
+          adb.setView(promptView);
+          final EditText textInput = (EditText) promptView.findViewById(R.id.userInput);
+          final EditText dateInput = (EditText) promptView.findViewById(R.id.dateInput);
+          adb
+             .setCancelable(false)
+             .setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface d, int id){
+                   dateText.setText(textInput.getText());
+                }
+             })
+             .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface d, int id) {
+                   d.cancel();
+                }
+             });
+          AlertDialog ad = adb.create();
+          ad.show();
+          
+          GregorianCalendar dayOf = Functions.toDate(dateText); //day user wants to find
+          mCursor.moveToFirst();
+          
+          //checks through all events
+          while(!mCursor.isAfterLast()){
+             if(!mCursor.getString(4).equals("1")){
+                GregorianCalendar start = new GregorianCalendar();
+                GregorianCalendar end = new GregorianCalendar();
+                start.setTimeInMillis(mCursor.getLong(1));
+                end.setTimeInMillis(mCursor.getLong(2)); // end date obj
+                boolean allDay = !mCursor.getString(3).equals("0"); //gets boolean if all day
+                Event event = new Event(mCursor.getString(0), start, end, allDay);
+                Log.d("MainActivity", "Event = " + (start.get(Calendar.MONTH)+1) + "/" + start.get(Calendar.DATE) + "/" + start.get(Calendar.YEAR));
+                if(Functions.checkIfDateMatch(event, dayOf)){
+                   Log.d("MainActivity", "Confirmed date");
+                   eventList.add(event);
+                }
+             }
+             mCursor.moveToNext(); //moves to next event
+          }
+          Collections.sort(eventList);
+          eventListCreated = true;
+     
+          //writes file
+          try {
+             Functions.writeToText(eventList);
+          } catch (IOException e) {
+          Toast.makeText(getApplicationContext(), "Error Writing File", Toast.LENGTH_LONG).show();
+          }
+          Toast.makeText(getApplicationContext(), "All Events Added", Toast.LENGTH_LONG).show();
+          enableBlu();
+          }
+          return true;
+          
+          // find option
+       case R.id.find:
+          if(eventListCreated)
+          {
+             ArrayList<Event> otherList = Functions.readFile();
+             boolean[] freeTime = Functions.freeTimeCalc(eventList, otherList);
+             
+             /*
+              * DEBUGGER DATA BELOW
+              */
+             String otherData = "";
+             Log.d("Other List Size", Integer.toString(otherList.size()));
+             for(int i = 0; i < otherList.size(); i++)
+             {
+                otherData += "Start: " + otherList.get(i).startDate.get(Calendar.HOUR_OF_DAY) + ":" + otherList.get(i).startDate.get(Calendar.MINUTE);
+                otherData += "  End:" + otherList.get(i).endDate.get(Calendar.HOUR_OF_DAY) + ":" + otherList.get(i).endDate.get(Calendar.MINUTE);
+                otherData += "\n";
+             }
+             Log.d("Other List", otherData);
+             
+             String eventData = "";
+             Log.d("Event List Size", Integer.toString(eventList.size()));
+             for(int i = 0; i < eventList.size(); i++)
+             {
+                eventData += "Start: " + eventList.get(i).startDate.get(Calendar.HOUR_OF_DAY) + ":" + eventList.get(i).startDate.get(Calendar.MINUTE);
+                eventData += "  End:" + eventList.get(i).endDate.get(Calendar.HOUR_OF_DAY) + ":" + eventList.get(i).endDate.get(Calendar.MINUTE);
+                eventData += "\n";
+             }
+             Log.d("Event List", eventData);
+             
+             int counter;
+             int startHours = 0;
+             int endHours = 0;
+             int startMinutes = 0;
+             int endMinutes = 0;
+             int streak = 0;
+             String freeTimeString = "";
+             for(counter = 0; counter < freeTime.length; counter++)
+             {
+                
+                if(freeTime[counter])
+                   streak++;
+                else
+                {
+                   if(streak == 0)
+                      continue;
+                   startHours = (counter-streak) / 60;
+                   startMinutes = (counter-streak) % 60;
+                   endHours = (counter) / 60;
+                   endMinutes = (counter) % 60;
+                   freeTimeString += "Start: " + startHours + ":" + startMinutes + "  End: " + endHours + ":" + endMinutes + "\n";
+                   streak = 0;
+                }
+             }
+             if(streak != 0)
+             {
+                startHours = (counter-streak) / 60;
+                startMinutes = (counter-streak) % 60;
+                endHours = (counter) / 60;
+                endMinutes = (counter) % 60;
+                freeTimeString += "Start: " + startHours + ":" + startMinutes + "  End: " + endHours + ":" + endMinutes + "\n";
+             }
+             Log.d("WRITETEST", freeTimeString);
+             FileWriter fWriter;
+             File sdCardFile = new File(Environment.getExternalStorageDirectory() + "/freeTime.txt");
+             Log.d("TAG", sdCardFile.getPath()); //<-- check the log to make sure the path is correct.
+             try{
+                  fWriter = new FileWriter(sdCardFile, false);
+                  fWriter.write(freeTimeString);
+                  fWriter.flush();
+                  fWriter.close();
+              }catch(Exception e){
+                       e.printStackTrace();
+              }
+             
+             intent = new Intent();
+             intent.setAction(Intent.ACTION_SEND);
+             intent.setType("text/plain");
+              intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/freeTime.txt")));
+              BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
+                   enableBlu();
+          }
+          else
+             Toast.makeText(this, "Generate Calendar Events First!", Toast.LENGTH_LONG).show();
+           return true;
+       case R.id.refresh:
+          
+           return true;
+       default:
+          break;
+       }
+       return super.onOptionsItemSelected(item);
+   } 
 }
